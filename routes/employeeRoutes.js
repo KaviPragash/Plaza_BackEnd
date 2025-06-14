373202f9904271281f4183f43e489fb94a410e9a
Kavipragash95@gmail.com <Kavipragash95@gmail.com>
2025-06-14 15:10:25 +0530

    Product API Updated

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -5,5 +5,8 @@ const Employee = require("../controllers/employeeController")
 
 router.post("/addEmployee", Employee.AddEmployee);
 router.get('/getallEmployee', Employee.GetAllEmployees);
+router.get('/GetEmployeeById/:id', Employee.GetEmployeeById);
+router.put('/UpdateEmployee/:id', Employee.UpdateEmployee);
+router.delete('/DeleteEmployee/:id', Employee.DeleteEmployee)
 
 module.exports = router;
